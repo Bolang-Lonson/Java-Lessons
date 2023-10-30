95b7342cf9fd78ad60987ee5ae7414a195f6b58f
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-31 00:46:53 +0100

    protected access modifier

--- src/mypack/B.java
+++ src/mypack/B.java
@@ -3,7 +3,14 @@ import pack.*;
 
 public class B {
     public static void main(String[] args) {
-        A obj = new A(); // Compile time error
+        C obj = new C(); // Compile time error
         obj.msg(); // Compile time error
+    }
+}
+
+class D extends A {
+    public static void main(String[] args) {
+        D obj = new D();
+        obj.msg();
     }
 }
--- src/pack/A.java
+++ src/pack/A.java
@@ -4,6 +4,15 @@
 
 package pack;
 
-class A { // No access modifier to the class hence treated as default
+class C { // No access modifier to the class hence treated as default
     void msg() {System.out.println("Hello!");}
+}
+
+/** 
+ * Protected access modifier is accessible within package and outside the package through inheritance only
+ * Protected access modifier can be applied on the data member, method and constructor but not on a class
+ */
+
+public class A {
+    protected void msg() {System.out.println("Hello!");}
 }
